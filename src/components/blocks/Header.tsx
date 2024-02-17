c9272f285e0706e744b4d34698d69aec5fa1cedd
a-belousovv <sasha.b2018@list.ru>
2024-02-17 13:57:17 +0300

    fix-header-lessons-text

--- src/components/blocks/Header.tsx
+++ src/components/blocks/Header.tsx
@@ -32,8 +32,9 @@ const Header = () => {
 								<div className='header__item_point'></div>
 								<div className='header__item_content'>
 									<h3 className='header__content_title'>
-										<span>320+ упражнений </span> пошаговые инструкции с
-										заданиями
+										<span>320+ упражнений </span> для дома, зала, стрейчинга и
+										работы с осанкой с пошаговыми видео и текстовыми
+										инструкциями
 									</h3>
 								</div>
 							</div>
@@ -41,9 +42,8 @@ const Header = () => {
 								<div className='header__item_point'></div>
 								<div className='header__item_content'>
 									<h3 className='header__content_title'>
-										<span>Комьюнити </span> <br /> чат с единомышленниками и
-										поддержкой + закрытый телеграм-канал с эксклюзивными
-										материалами
+										<span>Комьюнити </span> чат с единомышленниками и поддержкой
+										+ закрытый телеграм-канал с эксклюзивными материалами
 									</h3>
 								</div>
 							</div>
